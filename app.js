aa4563af2c35e1e1c251020296d7e6dccf55ffd0
zeba27 <zebasotota71318@gmail.com>
2024-04-10 00:52:38 +0600

    delete done

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ module.exports = app;
 
 
 const { MongoClient, ObjectId } = require("mongodb");
-const uri = "mongodb+srv://zewaso4950:TJI7jEnXLf2ZCi1q@cluster0.49awfao.mongodb.net/";
+const uri = "mongodb://localhost:27017/";
 const client = new MongoClient(uri);
 
 
@@ -53,3 +53,39 @@ app.post('/blogs', async (req, res, next) => {
       //await client.close();
     }
 });
+
+
+app.delete('/blogs/:id', async (req, res, next) => {
+    const { id: blogIdToDelete } = req.params;
+    try {
+        await client.connect();
+        const database = client.db('blog');
+        const blogCollection = database.collection('blogs');
+        const query = {"_id": new ObjectId(blogIdToDelete)};
+        const deleteResult = await blogCollection.deleteOne(query);
+        return res.status(200).json(deleteResult);
+    } finally {
+        await client.close();
+    }
+});
+
+
+
+app.post('/users', async (req, res, next) => {
+    const { userName, email, password} = req.body;
+    try {
+        await client.connect();
+        const database = client.db('blog');
+        const userCollection = database.collection('users');
+        
+        const blog = await userCollection.insertOne({
+            userName,
+            email,
+            password
+        });
+  
+        return res.status(200).json(blog);
+      } finally {
+        //await client.close();
+      }
+  });git
